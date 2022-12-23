e6f733356877766b40937ee9ed6a2b4c6e303ade
lg519 <lg519@ic.ac.uk>
2022-12-23 12:59:34 +0000

    added stripe api integration

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const cors = require("cors");
 const express = require("express");
+
 // TODO: add a stripe key
-// const stripe = require("stripe")("");
+const stripe = require("stripe")(`${process.env.STRIPE_API_SECRET_KEY}`);
 const uuid = require("uuid");
 
 const app = express();
@@ -14,5 +15,35 @@ app.use(cors());
 
 //routes
 app.get("/", (req, res) => res.send("HEYAA"));
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log(`PRODUCT: ${product}`);
+  console.log(`PRICE: ${product.price}`);
+
+  // ensures user does not get charged twice for the same product
+  const idempotencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100, // prices in Stripe are in cents
+          currency: "GBP",
+          customer: customer.id,
+          recipt_email: customer.email,
+          description: ` purchase of ${product.name}`,
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
+
 //listen
 app.listen(8282, () => console.log(`LISTENING AT PORT 8282`));
