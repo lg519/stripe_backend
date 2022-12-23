4d24d37c8ec549b38caf9fb90ebcc4a119852781
lg519 <lg519@ic.ac.uk>
2022-12-23 10:18:54 +0000

    simple server running

---
+++ index.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+// TODO: add a stripe key
+// const stripe = require("stripe")("");
+const uuid = require("uuid");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => res.send("HEYAA"));
+//listen
+app.listen(8282, () => console.log("LISTENING AT PORT 8282"));
